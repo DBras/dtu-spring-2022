80e46f9f32438f3dfc0e35b1f27410cd8921e0da
McDannel <danielbrasholt@gmail.com>
2022-01-09 16:22:25 +0100

    Add comments to main

--- src/project/Main.java
+++ src/project/Main.java
@@ -5,20 +5,21 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-    public static void main(String[] args) {
+	public static final int PORT_NUMBER = 8080; // Variable for port number
+    public static void main(String[] args) { // Run on start
         ServerSocket server_sock = null;
         boolean server_active = true;
-        try {
-            server_sock = new ServerSocket(8080);
-            while (server_active) {
-                Socket client_socket = server_sock.accept();
+        try { 
+            server_sock = new ServerSocket(PORT_NUMBER); // Open server on port PORT_NUMBER
+            while (server_active) { // Run as long as server_active is true
+                Socket client_socket = server_sock.accept(); // Accept client connection
 
                 ClientRunnable new_client = new ClientRunnable(client_socket);
                 Thread client_thread = new Thread(new_client);
-                client_thread.start();
+                client_thread.start(); // Start new thread when client connects
             }
 
-        } catch (IOException e) {
+        } catch (IOException e) { // Catch error in opening server socket
             e.printStackTrace();
         }
     }
