25cb1006e1661593c59be02e8382d1fdaa82e6ad
McDannel <danielbrasholt@gmail.com>
2022-01-10 11:18:30 +0100

    Add Card and Deck classes with shuffle and initRandom methods

---
+++ src/project/Card.java
@@ -0,0 +1,36 @@
+package project;
+
+import java.util.Arrays;
+
+public class Card implements Comparable<Card>{
+    private String suit;
+    private int card_value;
+    private final String[] ACCEPTED_SUITS = {"Diamonds", "Clubs", "Hearts", "Spades"};
+
+    public Card(String suit, int card_value) {
+        if (is_valid(suit, card_value)) {
+            this.suit = suit;
+            this.card_value = card_value;
+        } else {
+            throw new IllegalArgumentException("Card parameters not valid");
+        }
+    }
+
+    private boolean is_valid(String suit, int card_value) {
+        return Arrays.asList(ACCEPTED_SUITS).contains(suit) && card_value > 0 && card_value <=13;
+    }
+
+    public int compareTo(Card c) {
+        if (this.card_value > c.card_value) {
+            return 1;
+        } else if (this.card_value < c.card_value) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    public String toString() {
+        return String.format("%s%d", this.suit.charAt(0), this.card_value);
+    }
+}
---
+++ src/project/Deck.java
@@ -0,0 +1,38 @@
+package project;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck {
+    private ArrayList<Card> deck_of_cards;
+
+    public Deck(ArrayList<Card> cards) {
+        this.deck_of_cards = cards;
+    }
+
+    public Deck() {
+        this.deck_of_cards = new ArrayList<Card>();
+        initRandomDeck();
+    }
+
+    private void initRandomDeck() {
+        String[] suits = {"Diamonds", "Clubs", "Hearts", "Spades"};
+        for (int i = 0; i < suits.length; i++) {
+            for (int j = 0; j < 13; j++) {
+                deck_of_cards.add(new Card(suits[i], j+1));
+            }
+        }
+    }
+
+    public void shuffle() {
+        Collections.shuffle(this.deck_of_cards);
+    }
+
+    public String toString() {
+        String out_string = "";
+        for (int i = 0; i < this.deck_of_cards.size(); i++) {
+            out_string += this.deck_of_cards.get(i) + " ";
+        }
+        return out_string;
+    }
+}
