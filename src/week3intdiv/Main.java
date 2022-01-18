e3ed47fd46df55631dec0ef97fc9c84a02423e35
McDannel <danielbrasholt@gmail.com>
2022-01-18 14:48:19 +0100

    Finish problem

--- src/week3intdiv/Main.java
+++ src/week3intdiv/Main.java
@@ -7,18 +7,16 @@ public class Main {
         String d = "11000111010";
         String g = "1001";
         int data_length = d.length(), gen_length = g.length();
-
         BigInteger D = new BigInteger(d,2);
         BigInteger G = new BigInteger(g, 2);
-
         BigInteger D_placeholder = new BigInteger(d, 2);
-        BigInteger G_placeholder = null;
+        BigInteger G_placeholder;
 
-        for (int i = 0; i < data_length; i++) {
-            int shift = data_length - gen_length - i;
-            G_placeholder = G.shiftLeft(shift);
-            System.out.println(D_placeholder.toString(2));
-            System.out.println(G_placeholder.toString(2));
+        for (int i = 0; i < data_length-gen_length-2; i++) {
+            G_placeholder = G.shiftLeft(D_placeholder.bitLength()-gen_length);
+            D_placeholder = D_placeholder.xor(G_placeholder);
         }
+        System.out.println(D.toString(2));
+        System.out.println(D_placeholder.toString(2));
     }
 }
